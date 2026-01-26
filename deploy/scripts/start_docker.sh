31863a638351e54c7982f64484080eca28215d29
Bhushan Bowlekar <bowlekarbhushan88@gmail.com>
2026-01-26 23:12:05 +0530

    chnage port from 8000:8000 to 80:8000 in start_docker.sh

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -18,7 +18,7 @@ sudo docker stop home_price_api || true
 sudo docker rm home_price_api || true
 
 echo "Starting new container..."
-sudo docker run -d -p 8000:8000 --name home_price_api \
+sudo docker run -d -p 80:8000 --name home_price_api \
 -e DAGSHUB_USER_TOKEN=875e26c303d4b004f67cb17f815ac0736597ca44 \
 546327345928.dkr.ecr.ap-south-1.amazonaws.com/home_price_prediction:latest
 

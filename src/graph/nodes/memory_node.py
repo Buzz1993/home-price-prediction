4153824d68a0b56307b3edf641df5d3fb2a51dfc
Bhushan Bowlekar <bowlekarbhushan88@gmail.com>
2026-06-17 17:37:30 +0530

    all running successfully

--- src/graph/nodes/memory_node.py
+++ src/graph/nodes/memory_node.py
@@ -1,41 +1,41 @@
-# ===============================
-# memory_node.py
-# ===============================
+# # ===============================
+# # memory_node.py
+# # ===============================
 
-from src.llm.memory_store import SQLiteMemoryStore
-from src.llm.deepseek_memory import extract_memory
+# from src.llm.memory_store import SQLiteMemoryStore
+# from src.llm.deepseek_memory import extract_memory
 
-memory_store = SQLiteMemoryStore()
+# memory_store = SQLiteMemoryStore()
 
-USER_ID = "default_user" # Static user ID - "default_user" used for this project because
-                         # multi-user support is not implemented yet.
-                         # Every time memory_node runs, it uses the same
-                         # user_id to store and retrieve memories.
+# USER_ID = "default_user" # Static user ID - "default_user" used for this project because
+#                          # multi-user support is not implemented yet.
+#                          # Every time memory_node runs, it uses the same
+#                          # user_id to store and retrieve memories.
 
 
-def memory_node(state):
-    print("✅ memory_node executed")
+# def memory_node(state):
+#     print("✅ memory_node executed")
 
-    user_msg = state["user_message"]
+#     user_msg = state["user_message"]
 
-    # -----------------------------
-    # EXTRACT NEW MEMORY
-    # -----------------------------
-    memory = extract_memory(user_msg)
+#     # -----------------------------
+#     # EXTRACT NEW MEMORY
+#     # -----------------------------
+#     memory = extract_memory(user_msg)
 
-    if memory:
-        memory_store.add_memory(USER_ID, memory)
+#     if memory:
+#         memory_store.add_memory(USER_ID, memory)
 
-    # -----------------------------
-    # LOAD ALL MEMORIES
-    # -----------------------------
-    memories = memory_store.get_memories(USER_ID)
+#     # -----------------------------
+#     # LOAD ALL MEMORIES
+#     # -----------------------------
+#     memories = memory_store.get_memories(USER_ID)
 
-    state["memory"] = memories
+#     state["memory"] = memories
 
-    return state
+#     return state
 
-==================================================================================================================================================================================
+#==================================================================================================================================================================================
 
 # ===============================
 # memory_node.py
